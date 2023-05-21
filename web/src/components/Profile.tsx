c4e27444dde9807a3c5e99377766686b86d07c49
Wilson Franca <wilson.franca.92@gmail.com>
2023-05-21 11:23:26 -0700

    feat(web): saved user info in token and showed user profile

---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
+
+export function Profile() {
+  const { name, avatarUrl } = getUser()
+  return (
+    <div className="group flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={40}
+        height={40}
+        alt="User image"
+        className="h-10 w-10 rounded-full"
+        draggable="false"
+      />
+
+      <p className="text-sm leading-snug">
+        {name}
+        <br />
+        <a
+          href=""
+          className="block text-red-400 transition-colors hover:text-red-300"
+        >
+          Logout
+        </a>
+      </p>
+    </div>
+  )
+}
