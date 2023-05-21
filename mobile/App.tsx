e2f4c31aa700e67f74e244640ce24a0c123e46fd
Wilson Franca <wilson.franca.92@gmail.com>
2023-05-20 20:38:12 -0700

    feat(mobile): added google fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="text-5xl font-bold text-gray-50">Hello</Text>
+      <Text className="font-title text-5xl text-gray-50">Hello</Text>
       <StatusBar style="light" translucent />
     </View>
   )
