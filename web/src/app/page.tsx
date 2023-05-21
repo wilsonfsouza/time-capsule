5f2382f5beb0af43f369d149fae7e17bc67fcebe
Wilson Franca <wilson.franca.92@gmail.com>
2023-05-20 18:16:24 -0700

    feat(web): extended color tokens in tailwindcss

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 ">
       <h1>Your time capsule</h1>
-      <h2>Test</h2>
+      <h2>Continue at 12:31 Aula 2</h2>
     </div>
   )
 }
