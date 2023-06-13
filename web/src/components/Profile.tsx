cd0ac2f63cfcec55a2a4e10e38fc3cac9a64d7e9
Wilson Franca <wilson.franca.92@gmail.com>
2023-06-12 19:52:20 -0700

    feat(web): added logout route and updated logout link

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -18,7 +18,7 @@ export function Profile() {
         {name}
         <br />
         <a
-          href=""
+          href="/api/auth/logout"
           className="block text-red-400 transition-colors hover:text-red-300"
         >
           Logout
