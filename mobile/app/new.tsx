c0cc749bb5706c0197122ab9dcc69cfaa80ac461
Wilson Franca <wilson.franca.92@gmail.com>
2023-06-19 18:56:18 -0700

    feat(mobile): created screen to save new memory

---
+++ mobile/app/new.tsx
@@ -0,0 +1,77 @@
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native'
+import { useState } from 'react'
+import { Switch } from 'react-native-gesture-handler'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Icon from '@expo/vector-icons/Feather'
+import { Link } from 'expo-router'
+
+import Logo from '../src/assets/logo.svg'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
+  const [isPublic, setIsPublic] = useState(false)
+
+  return (
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <Logo />
+        <Link href={'/memories'} asChild>
+          <TouchableOpacity className="rounder full h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+            <Icon name="arrow-left" size={16} color="#fff" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+
+      <View className="mt-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch
+            value={isPublic}
+            onValueChange={setIsPublic}
+            thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+            trackColor={{ false: '#767577', true: '#372560' }}
+          />
+
+          <Text className="font-body text-base text-gray-200">
+            Make memory public
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
+          <View className="flex-row items-center gap-2">
+            <Icon name="image" color="#fff" />
+            <Text className="font-body text-sm text-gray-200">
+              Add cover photo or video
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TextInput
+          multiline
+          className="p-0 font-body text-lg text-gray-50"
+          placeholderTextColor="#56565a"
+          placeholder="Use this space to add pictures, videos, and a description of the experience you want to save for the future."
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="items-center self-end rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">Save</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  )
+}
