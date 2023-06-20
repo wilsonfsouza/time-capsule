0cf05fc8266063962b2d7a241f24ba9fb5689f9d
Wilson Franca <wilson.franca.92@gmail.com>
2023-06-19 18:12:46 -0700

    feat(mobile): personalized initial route based on user being authenticated or not

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -13,15 +13,27 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import blugBackground from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
 import { SplashScreen, Stack } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
+import { useEffect, useState } from 'react'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) {
     return <SplashScreen />
@@ -42,7 +54,10 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
